5be0e8ef8ff963df5ad96214ce1236034532366c
Joe Burgess <josephburgess@gmail.com>
2023-02-19 20:05:25 +0200

    adding first basic tests

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -1,9 +1,29 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+  describe('basic items', () => {
+    it('should initialise with an empty array', () => {
+      const gildedRose = new GildedRose()
+      expect(gildedRose.items).toEqual([])
+    });
+    it('should add a single item, foo', () => {
+      const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].name).toBe('foo');
+    });
+    it('should degrade the value and sellin of a regular item by one after a day', () => {
+      const gildedRose = new GildedRose([new Item("+5 Dexterity Vest", 10, 20)])
+      gildedRose.updateQuality();
+      expect(gildedRose.items[0].quality).toBe(19)
+      expect(gildedRose.items[0].sellIn).toBe(9)
+    });
+    it('should degrade the value and sellin of a regular item by two after two days', () => {
+      const gildedRose = new GildedRose([new Item("+5 Dexterity Vest", 10, 20)])
+      gildedRose.updateQuality();
+      gildedRose.updateQuality();
+      expect(gildedRose.items[0].quality).toBe(18)
+      expect(gildedRose.items[0].sellIn).toBe(8)
+    });
   });
+  
 });
--- test/mocha/gilded-rose.spec.ts
+++ test/mocha/gilded-rose.spec.ts
@@ -2,9 +2,18 @@ import { expect } from 'chai';
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal('foo');
+  describe('items', () => {
+    
+    it('should initialise with an empty array for items', () => {
+      const gilded
+    });
+
+    it('should add foo to the inventory', () => {
+      const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].name).to.equal('foo');
+    });
   });
+  
+
 });
