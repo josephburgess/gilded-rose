0fe55c4d66f2e49697a0680c2315a2de775160d5
Joe Burgess <josephburgess@gmail.com>
2023-02-20 07:59:45 +0200

    second helper method

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -29,16 +29,7 @@ export class GildedRose {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
+            this.updateConcertTickets(this.items[i])
           }
         }
       }
@@ -69,5 +60,14 @@ export class GildedRose {
 
   updateNormalItem(item: Item){
     item.quality --
+  }
+
+  updateConcertTickets(item: Item){
+      if (item.sellIn < 11 && item.quality < 50){
+        item.quality ++
+      }
+      if (item.sellIn < 6 && item.quality < 50){
+        item.quality ++
+      }
   }
 }
