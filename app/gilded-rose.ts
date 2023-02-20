53e430c827c424e9cf8d79d5314d12cfd98094f0
Joe Burgess <josephburgess@gmail.com>
2023-02-20 11:20:04 +0200

    all helper methods added

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -19,53 +19,60 @@ export class GildedRose {
 
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.updateNormalItemQuality(this.items[i])
-          }
+      if (
+        this.items[i].name != 'Aged Brie' &&
+        this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert'
+      ) {
+        if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+          this.updateNormalItemQuality(this.items[i]);
+        }
       } else {
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            this.updateConcertTicketsQuality(this.items[i])
-          } else if (this.items[i].name == 'Aged Brie') {
-            this.updateAgedBrieQuality(this.items[i])
-           }
+        if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+          this.updateConcertTicketsQuality(this.items[i]);
+        } else if (this.items[i].name == 'Aged Brie') {
+          this.updateAgedBrieQuality(this.items[i]);
+        }
       }
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
         this.adjustSellIn(this.items[i]);
       }
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (
+          this.items[i].name != 'Aged Brie' &&
+          this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert'
+        ) {
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.updateNormalItemQuality(this.items[i]) 
-              }
-            } else {
-              // this.updateAgedBrieQuality(this.items[i])
-            }
+            this.updateNormalItemQuality(this.items[i]);
+          }
+        } else {
+          // this.updateAgedBrieQuality(this.items[i])
+        }
       }
     }
 
     return this.items;
   }
 
-  private decreaseQuality(item: Item){
+  private decreaseQuality(item: Item) {
     if (item.quality > 0) {
-    item.quality --
+      item.quality--;
     }
   }
 
-  private increaseQuality(item: Item, amount: number = 1){
-    if(item.quality < 50)
-    item.quality = Math.min(item.quality + amount, 50)
+  private increaseQuality(item: Item, amount: number = 1) {
+    if (item.quality < 50) item.quality = Math.min(item.quality + amount, 50);
   }
 
-  private updateNormalItemQuality(item: Item){
-    this.decreaseQuality(item)
+  private updateNormalItemQuality(item: Item) {
+    this.decreaseQuality(item);
   }
 
-  private updateAgedBrieQuality(item: Item){
-    if (item.sellIn < 0){
-    this.increaseQuality(item, 2)}
-    else{this.increaseQuality(item)}
+  private updateAgedBrieQuality(item: Item) {
+    if (item.sellIn < 0) {
+      this.increaseQuality(item, 2);
+    } else {
+      this.increaseQuality(item);
+    }
   }
 
   private updateConcertTicketsQuality(item: Item) {
@@ -79,10 +86,9 @@ export class GildedRose {
       this.increaseQuality(item);
     }
   }
-  
 
-  private adjustSellIn (item: Item) {
-    item.sellIn --
+  private adjustSellIn(item: Item) {
+    item.sellIn--;
   }
 }
 
